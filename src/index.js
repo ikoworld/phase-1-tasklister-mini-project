280065f01f0f7de7854386d4e43c4fa9d3d30adf
aboura <bouchrabichou638@gmail.com>
2023-08-13 11:38:38 +0200

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form =document.querySelector("form");
+const input=document.querySelector("#new-task-description");
+const btn=document.createElement("button");
+form.addEventListener("submit",function(e){
+  e.preventDefault();
+
+  const li =document.createElement('li');
+  const newTodo =input.value;
+
+  li.textContent=`${newTodo} `;
+ 
+  btn.textContent="x"
+  li.appendChild(btn);
+  
+  const ul =document.querySelector('#tasks');
+  ul.appendChild(li);
+  form.reset();
+
+})
+function deleteButton() {
+  btn.parentElement.remove();
+}
+btn.addEventListener("click",deleteButton);
+
 });
+
+
